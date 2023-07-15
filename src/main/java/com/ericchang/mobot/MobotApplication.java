24697f3a03aecf994aa49cb260120bdc03103148
Eric Chang <>
2023-07-15 15:34:48 +0800

    init project

---
+++ src/main/java/com/ericchang/mobot/MobotApplication.java
@@ -0,0 +1,13 @@
+package com.ericchang.mobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericchang/mobot/MobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericchang.mobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
